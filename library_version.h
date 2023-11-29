3904fefdd968470f9e63a6d9e1f2b7c73095fea9
jenkins <jenkins@jenkins.multitech.net>
2023-11-29 07:37:52 -0600

    xdotad-library revision 4.2.6 and mbed-os revision mbed-os-6.17.0-rc3-90-g72f27cee92

--- library_version.h
+++ library_version.h
@@ -1,6 +1,6 @@
 #ifndef __LIBRARY_VERSION_H__ 
 #define __LIBRARY_VERSION_H__ 
-#define MDOT_VERSION "4.2.5-1-g33d2f92" 
+#define MDOT_VERSION "4.2.6" 
 #define LW_VERSION "1.0.4" 
 #define RP_VERSION "1.0.3" 
 #endif 
